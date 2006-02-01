ff9211198342700100b328a2815f7697d989ecf4
Dustin Sallings <dustin@spy.net>
2006-02-01 07:53:34 +0000

    added pubdate and a few more rss attributes

--- apps/blog/feeds.py
+++ apps/blog/feeds.py
@@ -10,19 +10,28 @@ from django.contrib.comments import feeds
 from django.models.blog import posts
 
 class Full(Feed):
-    title = "Full Posts"
+    title = "Noelani's Blog - Full Posts"
     link = "/"
     description = "Stuff that's on Noelani's mind"
+
+    author_name = 'Noelani Sallings'
+
+    author_email = 'noelani@spy.net'
+
+    author_link = 'http://bleu.west.spy.net/~noelani/'
+
+    def item_pubdate(self, item):
+        return item.post_date
     
     def items(self):
         return posts.get_list(released__exact=True,
             order_by=('-post_date', '-id'), limit=10)
 
 class Summary(Full):
-    title = "Post Summaries"
+    title = "Noelani's Blog - Post Summaries"
 
 class Unreleased(Full):
-    title = "Unreleased"
+    title = "Noelani's Blog - Unreleased"
     
     def items(self):
         return posts.get_list(order_by=('-post_date', '-id'), limit=10)
